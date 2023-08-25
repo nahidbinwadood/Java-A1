b7bbf7d0b8bf6624f59b28f94d02a8372544c1e6
nahidbinwadood <142960051+nahidbinwadood@users.noreply.github.com>
2023-08-25 18:57:34 +0600

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,62 @@
+// ১. Variable কি ?
+//variable is a name of storing data in javascript.
+
+// ২. Variable কিভাবে লিখতে হয় ?
+ //Ans:starting with variable, var num1=29;
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+//ans: String type variable is that a kind of variable where the input should be written within a (``/''/"").Here the input can be anything ex- number,text etc .
+
+//  var num1="233";
+//  console.log(num1/typeof num1);
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+//ans: String type variable is that a kind of variable where the input should be written without a (``/''/"").Here the input only has to be a number  .
+
+ //exp=
+ //var num1=233;
+ //console.log(num1/typeof num1);
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+//There are only two types of variable in boolean type variable which is "true" and "false".These true and false have to write after declearing the variable without a (``/''/"").
+// exp= 
+// var BoolTxt=false;
+//  console.log(typeof BoolTxt);
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+//toUpperCase() works for making all the letters big which stored in variable and toLowerCase() works for making all the letters small of variable data.
+// exp= 
+// var para="my name is nahid";
+// console.log(para.toUpperCase());
+//exp=
+// var para="NAHID BIN WADOOD";
+// console.log(para.toLowerCase());
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+//ANS: There are four types of operation in Javascript which are Arithmatic(+ - * /), Comparison Operators(== === != !== > >= < <=), Bitwise (&, | ^ ~ << >> >>>) and Logical Operators(&& || !)
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন ।
+//ANS: var num1=-1030;
+// console.log(Math.abs(num1)); 
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+//Ans:
+// var num1=10.00001;
+// console.log(Math.ceil(num1)); 
+
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+//Ans:
+//var num1=10.999;
+// console.log(Math.floor(num1)); 
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+//Ans:
+// var num1=10.00001;
+// console.log(Math.round(num1)); 
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+//Ans
+//  var num=232;  
+// console.log(Math.random(num)); 
+// asignment.js - GitHub Link 
